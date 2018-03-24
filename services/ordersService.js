16df7643b821ca73afe87a9d5691ae9c02bfaae9
matheuspavin <pavinmatheus@gmail.com>
2018-03-24 12:56:01 -0300

    Inserting and retrieving

--- services/ordersService.js
+++ services/ordersService.js
@@ -1,6 +1,7 @@
+'use strict';
 const databaseService = require('./databaseService');
 
-const insertOrder = function (order) {
+const insertOrder = async function (order) {
     const sql = `INSERT INTO orders 
                         (order_id, company_name, customer_address, ordered_item, price, currency)
                     VALUES
@@ -13,15 +14,33 @@ const insertOrder = function (order) {
                         order.price,
                         order.currency
                     ]
-    databaseService.query(sql, params);
-}
+    //TODO Refactor this return, because it can be done on the insert sql
+    try {
+        await databaseService.query(sql, params);
+        const insertedId = await databaseService.query(`SELECT last_insert_rowid()as lastId`, []);
+        return getOrderById(insertedId[0].lastId);
+    } catch (error) {
+        return error;
+    }
+};
 
-
-const getAll = function () {
+const getAll = async function () {
     const sql = `SELECT * FROM orders`;
-    return databaseService.query(sql, []);
-} 
+    let ret;
+    try {
+        ret = await databaseService.query(sql, []);
+    } catch (error) {
+        return error
+    }
+    return ret;
+};
 
+const getOrderById = function (orderId) {
+    const sql = `SELECT * FROM ORDERS
+                 WHERE order_id = ?`;
+    const params = [orderId];
+    return databaseService.query(sql, params);
+}
 
 module.exports = {
     insertOrder,
